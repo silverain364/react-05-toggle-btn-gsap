ffc798609704e1cb432bf6336609cdde300ed372
silverain364 <134907634+silverain364@users.noreply.github.com>
2025-03-25 10:37:40 +0900

    Add files via upload

---
+++ src/components/Ani.jsx
@@ -0,0 +1,36 @@
+import React, {useRef, useState} from 'react'
+import {gsap} from 'gsap'
+import './Ani.scss'
+
+const Ani = () => {
+
+    const boxRef = useRef(null)
+    const [moved, setMoved] = useState(false)
+
+
+    const animateBox=()=>{
+        if(!moved){
+            gsap.to('boxRef', current, {
+                x:200,
+                backgroundColor:'#4dabf7',
+                duration: 1
+            })
+        }
+        else{
+            gsap.to('boxRef', current, {
+                x:0
+            })
+        }
+        setMoved(!moved)
+    }
+
+
+  return (
+    <div className='gsap-container'>
+        <div className="box" ref={boxRef}></div>
+        <button>실행</button>
+    </div>
+  )
+}
+
+export default Ani
